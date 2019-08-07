72cb3e96b6280d34fb998b4df0c095f4f201bcb0
Kaias26 <ryuran1@msn.com>
2019-08-07 15:18:14 +0200

    Ajout de la page d'accueil

---
+++ pages/home.php
@@ -0,0 +1,67 @@
+<div class="starter-template">
+	<h1 class="mt-5">Le donjon de Naheulbeuk - Compendium pour Roll20</h1>
+	<p class="lead">Compendium des differents tableaux du JDR papier Le donjon de naheulbeuk.<br> Compatible avec les fiches de personnages de la table de jeu en ligne Roll20.</p>
+</div>
+
+<div class="container">
+
+	<!-- Three columns of text below the carousel -->
+	<div class="row">
+		<div class="col-lg-3">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title"><i class="fas fa-book"></i> Grimoires</h5>
+					<p class="card-text">Liste des sorts magiques et prodiges karmiques</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item"><a href="/grimoires/mage" class="card-link">Mage</a></li>
+					<li class="list-group-item"><a href="/grimoires/paladin" class="card-link">Prêtre</a></li>
+					<li class="list-group-item"><a href="/grimoires/pretre" class="card-link">Paladin</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-3">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title"><i class="fas fa-book-dead"></i> PNJ</h5>
+					<p class="card-text">Pour bien préparer ses rencontres</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item"><a href="/pnj/invocations" class="card-link">Invocations</a></li>
+					<li class="list-group-item"><a href="/pnj/bestiaire" class="card-link">Bestiaire</a></li>
+					<li class="list-group-item"><a href="/pnj/pretires" class="card-link">Pré-tirés</a></li>
+					<li class="list-group-item"><a href="/pnj/traitsspeciaux" class="card-link">Traits spéciaux</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-3">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title"><i class="fas fa-portrait"></i> PJ</h5>
+					<p class="card-text">Pour créer et compléter son personnage</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item"><a href="/pj/competences" class="card-link">Compétences</a></li>
+					<li class="list-group-item"><a href="/pj/coupsspeciaux" class="card-link">Coups spéciaux</a></li>
+					<li class="list-group-item"><a href="/pj/traitsparticuliers" class="card-link">Traits particuliers</a></li>
+					<li class="list-group-item"><a href="/pj/ape" class="card-link">APE</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-3">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title"><i class="fas fa-toolbox"></i> Objets</h5>
+					<p class="card-text">Pour s'équiper avant de partir à l'aventure</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item"><a href="/objets/traitsspeciaux" class="card-link">Armement</a></li>
+					<li class="list-group-item"><a href="/objets/protections" class="card-link">Protections</a></li>
+					<li class="list-group-item"><a href="/objets/accessoires" class="card-link">Accessoires</a></li>
+					<li class="list-group-item"><a href="/objets/materiel" class="card-link">Matériel</a></li>
+					<li class="list-group-item"><a href="/objets/ingredients" class="card-link">Ingrédients</a></li>
+				</ul>
+			</div>
+		</div>
+	</div><!-- /.row -->
+</div>
