81884352c36dd71a13f38f19a2e2564a4ec46869
Kaias <ryuran1@msn.com>
2021-12-26 23:21:33 +0100

    Rajout de la page des Pré-tirés

---
+++ pages/pnj/pretires.php
@@ -0,0 +1,57 @@
+<div class="starter-template">
+	<h1 class="mt-5">Les Personnages pré-tirés</h1>
+	<p class="lead">Pack de personnages bas niveau<br>Source : <a href="http://naheulbeuk.com/doc-magie.htm" target="_bank">https://www.naheulbeuk.com/doc-aidesdejeu.htm#Pretires</a></p>
+</div>
+
+<ul class="nav nav-tabs">
+	<li class="nav-item">
+		<a class="nav-link <?php if( $subgroup == "officiels" ) {?>active<?php }?>" href="/pnj/pretires/officiels">Officiels</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link <?php if( $subgroup == "contributions" ) {?>active<?php }?>" href="/pnj/pretires/contributions">Contributions</a>
+	</li>
+</ul>
+
+<?php if( isset( $subgroup ) and $subgroup != "" ) {?>
+<br>
+<div class="container">
+	<table class="table table-hover table-striped" id="myDatatable">
+		<thead class="thead-dark">
+		<tr>
+			<th scope="col">Nom</th>
+			<th scope="col">Origine</th>
+			<th scope="col">Métier</th>
+			<th scope="col">Niveau</th>
+			<th scope="col">Source</th>
+			<th scope="col">Roll20</th>
+			<th scope="col"></th>
+		</tr>
+		</thead>
+		<tbody>
+			<?php
+			foreach ($aRows as $row ) {
+
+				echo "<tr>";
+					echo "<td>" . $row['name'] . "</td>";
+					echo "<td>" . $row['origine'] . "</td>";
+					echo "<td>" . $row['metier'] . "</td>";
+					echo "<td>" . $row['niveau'] . "</td>";
+					echo "<td>" . $row['source'] . "</td>";
+					echo "<td class='cell-min-width'>";
+					if( $row['roll20'] != "" ) {
+						echo "<input type='text' id='text_" . $row['id'] . "' class='form-control' value='" . $row['roll20'] . "' />";
+					}
+					echo "</td>";
+					echo "<td class='cell-min-width'>";
+					if( $row['roll20'] != "" ) {
+						echo "<button type='button' class='btn btn-default' data-id='" . $row['id'] . "' title='Copier'><i class='fa fa-copy'></i></button></td>";
+					}
+					echo "</td>";
+				echo "</tr>";
+			}
+			?>
+		</tbody>
+	</table>
+	<br>
+</div>
+<?php }?>
