acd239f2bf57613ee45ee7398e7c71f09ffadc25
Kaias <ryuran1@msn.com>
2021-12-27 19:51:32 +0100

    Upgrade bootstrap 5

--- includes/footer.php
+++ includes/footer.php
@@ -1,13 +1,13 @@
 <footer class="footer">
 	<div class="container text-center">
-		<span class="text-center text-muted">Le donjon de Naheulbeuk - &copy;<a href="http://www.naheulbeuk.com/" target="_blank">Pen of Chaos</a> <br> Compendium pour Roll20 - &copy;<a href="http://www.naheulbeuk-online.org/" target="_blank">Naheulbeuk Online</a> <?php print date("Y");?></span>
+		<span class="text-center text-muted">Le donjon de Naheulbeuk - &copy;<a href="https://www.naheulbeuk.com/" target="_blank">Pen of Chaos</a> <br> Compendium pour Roll20 - &copy;<a href="https://www.naheulbeuk-online.org/" target="_blank">Naheulbeuk Online</a> <?php print date("Y");?></span>
 	</div>
 </footer>
 
 <!-- Bootstrap core JavaScript  -->
 <script src="/js/jquery-3.4.1.min.js"></script>
 <script src="/js/popper.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
+<script src="/js/bootstrap/bootstrap.bundle.min.js"></script>
 <script src="/js/jquery.dataTables.min.js"></script>
 <script>
 	$(document).ready( function () {
--- includes/head.php
+++ includes/head.php
@@ -7,8 +7,8 @@
 <meta name="keywords" content="<?php echo $page_key;?>" />
 
 <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" rel="stylesheet">
+<link href="/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+<link href="/css/jquery.dataTables.min.css" rel="stylesheet">
 <link href="/css/fa/css/all.css" rel="stylesheet">
 <link href="/css/styles.css" rel="stylesheet" >
 
