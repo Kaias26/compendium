1a3ded980ab772255a362d267577c6922122a5cd
Kaias <ryuran1@msn.com>
2021-12-28 19:17:16 +0100

    Inverse les colonnes Name et Menace dans le bestiaire

--- pages/pnj/bestiaire.php
+++ pages/pnj/bestiaire.php
@@ -9,8 +9,8 @@
 		<tr>
 			<th scope="col">Type</th>
 			<th scope="col">Localisation</th>
-			<th scope="col">Menace</th>
 			<th scope="col">Nom</th>
+			<th scope="col">Menace</th>
 			<th scope="col">Notes</th>
 			<th scope="col">Roll20</th>
 			<th scope="col"></th>
@@ -23,6 +23,7 @@
 				echo "<tr>";
 					echo "<td>" . $row['type'] . "</td>";
 					echo "<td>" . $row['localisation'] . "</td>";
+					echo "<td class='cell-min-width'>" . $row['name'] . "</td>";
 					echo "<td class='cell-min-width'>";
 					if( $row['menace'] == 'faible' ) {
 						echo "<span class='d-none'>1</span>";
@@ -38,7 +39,6 @@
 						echo "<span class='fa fa-skull'></span>";
 					}
 					echo "</td>";
-					echo "<td class='cell-min-width'>" . $row['name'] . "</td>";
 					echo "<td>" . $row['notes'] . "</td>";
 					echo "<td class='cell-min-width'>";
 					if( $row['roll20'] != "" ) {
@@ -69,7 +69,7 @@
 			{ "targets": 1,"visible": false },
 			{ "orderable": false, "targets": [ -1, -2 ] }
 		],
-		"order": [[ 0, "asc" ], [ 2, "asc" ]],
+		"order": [[ 0, "asc" ], [ 3, "asc" ]],
 		"initComplete": function (settings, json) {
 			var $column = this.api().column( 1 );
 			var $div = $('<div class="dataTables_length" id="myDatatable_length"><label id="myDatatable_typefilter">Localisation : </label></div>')
