ab0641bd6561bdedb1081f4ff74c627617a0ba76
Kaias <ryuran1@msn.com>
2021-12-28 19:17:29 +0100

    Ajout de page FAQ

---
+++ pages/faq.php
@@ -0,0 +1,42 @@
+<div class="starter-template">
+	<h1 class="mt-5">Mode d'emploi</h1>
+</div>
+
+<div class="container">
+	<p class="lead">Le compendium Naheulbeuk est une base de données d'informations, telles que les Sorts, les Objets, les Monstres pour le jeu de role en ligne du <a href="https://www.naheulbeuk.com/" target="_blank">Donjon de Naheulbeuk</a> et la table virtuelle <a href="https://roll20.net/" target="_blank">Roll20</a>.<br>
+	En utilisant la feuille de personnage Roll20 du Donjon de Naheulbeuk, vous pourrez, via ce compendium importer divers élements directement dans votre partie.</p>
+
+	<div class="row">
+		<div class="col-lg-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title"><i class="fa fa-cog"></i> Importer une fiche</h5>
+					<p class="card-text">Créer une nouvelle fiche avec les informations complètes d'un PJ / PNJ</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">
+						<br>
+						- Copier le contenu de la cellule dans la colonne intitulée "Roll20" ou utiliser le bouton <i class="fa fa-copy"></i> qui va automatiquement copier la cellule (si votre navigateur internet le permet).<br><br>
+						- Coller le texte dans l'outil d'import de la fiche Roll20, accessible depuis le bouton <i class="fa fa-cog"></i>, en haut à droite de la feuille.<br><br>
+						<span style="color: #f00">Attention</span>, toutes les données de la fiche seront écrasées
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-6">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title"><i class="fa fa-tools"></i></i> Ajouter un element</h5>
+					<p class="card-text">Compléter une fiche existante avec un sort, un talent, un objet, une maladie (...)</p>
+				</div>
+				<ul class="list-group list-group-flush">
+					<li class="list-group-item">
+						<br>
+						- Copier le contenu de la cellule dans la colonne intitulée "Roll20" ou utiliser le bouton <i class="fa fa-copy"></i> qui va automatiquement copier la cellule (si votre navigateur internet le permet).<br><br>
+						- Coller le texte dans la pop-up de la fiche Roll20, accessible depuis le bouton <i class="fa fa-tools"></i>, en haut à droite de la feuille.
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div><!-- /.row -->
+</div>
