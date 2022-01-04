47d4fb93e0c0fd2733d03cf3efd4b80df9103827
Kaias <ryuran1@msn.com>
2022-01-04 21:39:40 +0100

    Rajout des compétences des classes "Suppléments"

--- pages/pj/competences.php
+++ pages/pj/competences.php
@@ -3,11 +3,25 @@
 	<p class="lead">Liste des compétences pour les personnages joueurs.<br>Source : <a href="http://www.naheulbeuk.com/jdr-docs/ecran-resumecompetences.pdf" target="_bank">http://www.naheulbeuk.com/jdr-docs/ecran-resumecompetences.pdf</a></p>
 </div>
 
+<ul class="nav nav-tabs">
+	<li class="nav-item">
+		<a class="nav-link <?php if( $subgroup == "classiques" ) {?>active<?php }?>" href="/pj/competences/classiques">Classiques</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link <?php if( $subgroup == "supplements" ) {?>active<?php }?>" href="/pj/competences/supplements">Suppléments</a>
+	</li>
+</ul>
+
+<?php if( isset( $subgroup ) and $subgroup != "" ) {?>
+<br>
 <div class="container">
 	<table class="table table-hover table-striped" id="myDatatable">
 		<thead class="table-dark">
 		<tr>
-			<th scope="col">Nom</th>
+			<?php if( $subgroup == "supplements" ) {?>
+			<th scope="col">Métier</th>
+			<?php }?>
+			<th scope="col">Nom</th>			
 			<th scope="col">Effet</th>
 			<th scope="col">Roll20</th>
 			<th scope="col"></th>
@@ -19,7 +33,10 @@
 				$macro = '{"repeating_competences_rowID_competence_nom":"' . $row['name'] . '","repeating_competences_rowID_competence_effet":"' . $row['effet'] . '","repeating_competences_rowID_competence_epreuve":"' . ( $row['jet'] == "-" ? 0 : 1) . '","repeating_competences_rowID_competence_jet":"' . $row['jet'] . '","repeating_competences_rowID_competence_mod":"' . $row['mod'] . '"}';
 
 				echo "<tr>";
-					echo "<td class='cell-min-width'>" . $row['name'] . "</td>";
+					if( $subgroup == "supplements" ) {
+						echo "<td class='cell-min-width'>" . $row['metier'] . "</td>";
+					}
+					echo "<td class='cell-min-width'>" . $row['name'] . "</td>";					
 					echo "<td>" . $row['effet'] . "</td>";
 					echo "<td class='cell-min-width'>";
 						echo "<input type='text' id='text_" . $row['id'] . "' class='form-control' value='" . $macro . "' />";
@@ -34,3 +51,4 @@
 	</table>
 	<br>
 </div>
+<?php }?>
