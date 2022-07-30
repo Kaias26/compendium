3f753e2a9ed360a144eb0d341938e05340ebfa76
Kaias <ryuran1@msn.com>
2022-07-30 23:20:29 +0200

    Update du Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="footer">
 	<div class="container text-center">
-		<span class="text-center text-muted">Le donjon de Naheulbeuk - &copy;<a href="https://www.naheulbeuk.com/" target="_blank">Pen of Chaos</a> <br> Compendium pour Roll20 - &copy;<a href="https://www.naheulbeuk-online.org/" target="_blank">Naheulbeuk Online</a> <?php print date("Y");?></span>
+		<span class="text-center text-muted">Le donjon de Naheulbeuk - &copy;<a href="https://www.naheulbeuk.com/" target="_blank">Pen of Chaos</a> <br> Compendium pour Roll20 - &copy;<a href="https://www.naheulbeuk-online.org/" target="_blank">Naheulbeuk Online</a> - &copy;<a href="https://discordapp.com/users/148057238680895488" target="_blank">Kaias</a> <?php print date("Y");?></span>
 	</div>
 </footer>
 
