92aae5133dae3f3b041c1c2c80cfe33b038366f7
Kaias <ryuran1@msn.com>
2023-04-13 19:00:24 +0200

    Ajout d'un logo dans la navbar

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,7 @@
 <header>
 	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
 		<div class="container-fluid">
-			<a class="navbar-brand" href="/">Naheulbeuk - Compendium</a>
+			<a class="navbar-brand" href="/"><img src="/img/naheulbeuk.png" alt="Naheulbeuk - Compendium" title="Naheulbeuk - Compendium" width="40" height="40"> Naheulbeuk - Compendium</a>
 			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 			<div class="collapse navbar-collapse" id="navbarCollapse">
 				<ul class="navbar-nav me-auto mb-2 mb-md-0">
