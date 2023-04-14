3b82b41eeba6972113686d0098793d70d290f5ce
Kaias <ryuran1@msn.com>
2023-04-14 10:54:30 +0200

    [Fix] Correction des btn de fermeture des modales

--- modals/coupspecial.php
+++ modals/coupspecial.php
@@ -4,9 +4,7 @@
 
 <div class="modal-header">
 	<h5 class="modal-title" id="modal-title"><?php echo $aRows[ 0 ]['name'] ?></h5>
-	<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-		<span aria-hidden="true">&times;</span>
-	</button>
+	<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
 <div class="modal-body">
 	<?php if( (isSet( $aRows[ 0 ]['niveau'] ) ) and ( $aRows[ 0 ]['niveau'] != "" or $aRows[ 0 ]['apprentissage'] != "" ) ) { ?>
@@ -50,5 +48,5 @@
 	<?php } ?>
 </div>
 <div class="modal-footer">
-	<button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+	<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
 </div>
