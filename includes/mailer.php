e78c42419899ef69a05080ea11283f6e153ed893
Kaias <ryuran1@msn.com>
2023-04-15 14:04:13 +0200

    [Fix] Ajout du param isHtml au service mailer

--- includes/mailer.php
+++ includes/mailer.php
@@ -14,6 +14,7 @@
 
 		$mail = new PHPMailer(true);  // Cree un nouvel objet PHPMailer
 		$mail->IsSMTP(); // active SMTP
+		$mail->IsHTML(true);
 		$mail->SMTPDebug = 0;  // debogage: 1 = Erreurs et messages, 2 = messages seulement
 		$mail->SMTPAuth = true;  // Authentification SMTP active
 		$mail->SMTPSecure = 'tls'; // Gmail REQUIERT Le transfert securise
