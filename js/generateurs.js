710c8028682e575331d2980bc1cef6deca3e7e1b
Kaias <ryuran1@msn.com>
2025-08-05 00:29:04 +0200

    Déplace le JS display_results dans la page concernée (Bijoux, Noms...)

--- js/generateurs.js
+++ js/generateurs.js
@@ -37,17 +37,3 @@ $(document).ready( function () {
 
 	setTimeout( function() { display_results(); }, 1500	);
 });
-
-function display_results() {
-	var _aResults = $("ul.list-group-flush");
-	var _bijoux = $( _aResults[0] ).children(":first").html();
-	var _qualite = $( _aResults[1] ).children(":first").html();
-	var _matiere = $( _aResults[2] ).children(":first").html();
-	var _gemme = $( _aResults[3] ).children(":first").html();
-	var _desc = $( _aResults[3] ).children(":first").data( "desc" );
-	var _gold = $( _aResults[3] ).children(":first").data( "gold" );
-	var _ug = $( _aResults[4] ).children(":first").html();
-
-	$( "#results" ).html( "Vous trouvez " + _bijoux + " " + _qualite + " " + _matiere + " serti " + _gemme + " (" +  _desc + "), de " +  _ug + " U.G" );
-	$( "#price" ).html( "Valeur de la gemme seule : " + ( _ug * _gold ) + " P.O (" + _ug + " x " + _gold + ").<br>Bonus ou Malus possible en fonction de la qualité et la matière du bijoux." );
-}
--- pages/tools/generateurBijoux.php
+++ pages/tools/generateurBijoux.php
@@ -361,3 +361,19 @@ $aBijouxMatiere = [ "en os", "en cuir", "en bois", "en cuivre", "en bronze", "en
 		<span id="price"></span>
 	</div>
 </div>
+
+<script>
+function display_results() {
+	var _aResults = $("ul.list-group-flush");
+	var _bijoux = $( _aResults[0] ).children(":first").html();
+	var _qualite = $( _aResults[1] ).children(":first").html();
+	var _matiere = $( _aResults[2] ).children(":first").html();
+	var _gemme = $( _aResults[3] ).children(":first").html();
+	var _desc = $( _aResults[3] ).children(":first").data( "desc" );
+	var _gold = $( _aResults[3] ).children(":first").data( "gold" );
+	var _ug = $( _aResults[4] ).children(":first").html();
+
+	$( "#results" ).html( "Vous trouvez " + _bijoux + " " + _qualite + " " + _matiere + " serti " + _gemme + " (" +  _desc + "), de " +  _ug + " U.G" );
+	$( "#price" ).html( "Valeur de la gemme seule : " + ( _ug * _gold ) + " P.O (" + _ug + " x " + _gold + ").<br>Bonus ou Malus possible en fonction de la qualité et la matière du bijoux." );
+}
+</script>
