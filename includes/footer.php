3ef0d87c24b2548f552d8e6d3004f1990bed16be
Kaias <ryuran1@msn.com>
2025-08-05 22:23:02 +0200

    Passe le JS dans le header au lieu du footer

--- includes/footer.php
+++ includes/footer.php
@@ -3,22 +3,3 @@
 		<span class="text-center text-muted">Le donjon de Naheulbeuk - &copy;<a href="https://www.naheulbeuk.com/" target="_blank">Pen of Chaos</a> <br> Compendium pour Roll20 - &copy;<a href="https://www.naheulbeuk-online.org/" target="_blank">Naheulbeuk Online</a> - &copy;<a href="https://discordapp.com/users/148057238680895488" target="_blank">Kaias</a> <?php print date("Y");?></span>
 	</div>
 </footer>
-
-<!-- Bootstrap core JavaScript  -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script src="https://cdn.datatables.net/v/bs5/dt-2.0.8/r-3.0.2/rg-1.5.0/datatables.min.js"></script>
-
-<?php if( $folder == 'charactersheet' ) {?>
-	<script src="/js/charactersheet.js?20220104"></script>
-<?php }?>
-
-<?php if( $folder == 'tools' && ( $group == "generateurBijoux" || $group == "generateurNoms" ) ) {?>
-	<script src="/js/generateurs.js?20220104"></script>
-<?php }?>
-
-<?php if( $folder == 'tools' && ( $group == "combatRapide" ) ) {?>
-	<script src="/js/combatRapide.js?20250805"></script>
-<?php }?>
-
-<script src="/js/functions.js?20220104"></script>
--- includes/head.php
+++ includes/head.php
@@ -12,18 +12,28 @@
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" rel="stylesheet" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link href="/css/styles.css" rel="stylesheet" >
 
-<?php if( $folder == 'charactersheet' ) {?>
-	<link href="/css/charactersheet.css" rel="stylesheet" >
-<?php }?>
-
-<?php if( $folder == 'tools' && ( $group == "generateurBijoux" || $group == "generateurNoms" ) ) {?>
-	<link href="/css/generateurs.css" rel="stylesheet" >
-<?php }?>
-
-<?php if( $folder == 'tools' && ( $group == "combatRapide" ) ) {?>
-	<link href="/css/combatRapide.css" rel="stylesheet" >
-<?php }?>
+<!-- Bootstrap core JavaScript  -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="https://cdn.datatables.net/v/bs5/dt-2.0.8/r-3.0.2/rg-1.5.0/datatables.min.js"></script>
 
 <script type="text/javascript">
 	var dataTables_options = {};
 </script>
+
+<?php if( $folder == 'charactersheet' ) {?>
+	<link href="/css/charactersheet.css" rel="stylesheet" >
+	<script src="/js/charactersheet.js?20220104"></script>
+<?php }?>
+
+<?php if( $folder == 'tools' && ( $group == "generateurBijoux" || $group == "generateurNoms" ) ) {?>
+	<link href="/css/generateurs.css" rel="stylesheet" >
+	<script src="/js/generateurs.js?20220104"></script>
+<?php }?>
+
+<?php if( $folder == 'tools' && ( $group == "combatRapide" ) ) {?>
+	<link href="/css/combatRapide.css" rel="stylesheet" >
+	<script src="/js/combatRapide.js?20250805"></script>
+<?php }?>
+
+<script src="/js/functions.js?20220104"></script>
